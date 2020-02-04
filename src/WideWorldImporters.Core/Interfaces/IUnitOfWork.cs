a3b062099bfe2172622965765bd79fec838ff222
Ibrahim Gokalp <gokalpi@gmail.com>
2020-02-04 11:08:21 +0300

    Added UnitOfWork pattern support

---
+++ src/WideWorldImporters.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WideWorldImporters.Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/WideWorldImporters.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WideWorldImporters.Core.Interfaces;
+
+namespace WideWorldImporters.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly WideWorldImportersContext _context;
+
+        public UnitOfWork(WideWorldImportersContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public virtual int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public virtual async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
