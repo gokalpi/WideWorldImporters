770a7ef1aed312e5b2597c61d62132ee89b81806
Ibrahim Gokalp <gokalpi@gmail.com>
2020-02-04 11:17:48 +0300

    Added logging infrastructure

---
+++ src/WideWorldImporters.Core/Interfaces/IAppLogger.cs
@@ -0,0 +1,11 @@
+﻿namespace WideWorldImporters.Core.Interfaces
+{
+    public interface IAppLogger<T>
+    {
+        void LogInformation(string message, params object[] args);
+
+        void LogWarning(string message, params object[] args);
+
+        void LogError(string message, params object[] args);
+    }
+}
---
+++ src/WideWorldImporters.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using WideWorldImporters.Core.Interfaces;
+
+namespace WideWorldImporters.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+    }
+}
